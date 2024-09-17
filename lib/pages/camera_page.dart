16e74b007c033afdbc9822349d18ff0c70b7bef8
hasinduK <it22234452@my.sliit.lk>
2024-09-17 18:37:07 +0530

    Text is scanned from image and display in a alert

--- lib/pages/camera_page.dart
+++ lib/pages/camera_page.dart
@@ -16,6 +16,60 @@ class _CameraPageState extends State<CameraPage> {
   late Future<void> _initializeControllerFuture;
   XFile? _imageFile;
   bool _isCapturing = false;
+  bool isImageCaptured = false;
+
+  void _retakePicture() {
+    setState(() {
+      _imageFile = null;
+      isImageCaptured = false;
+    });
+  }
+
+  void _analyzeImage() async {
+    if (_imageFile != null) {
+      try {
+        // Load the image from file
+        final inputImage = InputImage.fromFilePath(_imageFile!.path);
+
+        // Create an instance of the text recognizer
+        final textRecognizer = GoogleMlKit.vision.textRecognizer();
+
+        // Process the image
+        final RecognizedText recognizedText =
+            await textRecognizer.processImage(inputImage);
+
+        // Handle the recognized text
+        final text = recognizedText.text;
+
+        // Show the recognized text in an alert dialog
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Recognized Text'),
+              content: SingleChildScrollView(
+                child: Text(text),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+
+        // Dispose of the recognizer
+        textRecognizer.close();
+      } catch (e) {
+        // Handle exceptions
+        print('Error analyzing image: $e');
+      }
+    }
+  }
 
   @override
   void initState() {
@@ -32,6 +86,8 @@ class _CameraPageState extends State<CameraPage> {
       );
       _initializeControllerFuture = _controller.initialize();
       setState(() {});
+    } on CameraException catch (e) {
+      print('CameraException: ${e.code}\nError Message: ${e.description}');
     } catch (e) {
       print('Error initializing camera: $e');
     }
@@ -64,6 +120,7 @@ class _CameraPageState extends State<CameraPage> {
     } finally {
       setState(() {
         _isCapturing = false;
+        isImageCaptured = true;
       });
     }
   }
@@ -76,30 +133,60 @@ class _CameraPageState extends State<CameraPage> {
       ),
       body: Column(
         children: [
-          Expanded(
-            child: FutureBuilder<void>(
-              future: _initializeControllerFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return CameraPreview(_controller);
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else {
-                  return Center(child: CircularProgressIndicator());
-                }
-              },
+          if (_imageFile == null)
+            Expanded(
+              child: FutureBuilder<void>(
+                future: _initializeControllerFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return Center(child: CameraPreview(_controller));
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                },
+              ),
             ),
-          ),
-          if (_imageFile != null) Image.file(File(_imageFile!.path)),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: _takePicture,
-              child: _isCapturing
-                  ? const CircularProgressIndicator()
-                  : const Text('Capture Image'),
+
+          // Display captured image if available
+          if (_imageFile != null)
+            Expanded(
+              child: Image.file(
+                File(_imageFile!.path),
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
             ),
-          ),
+          // Capture Image Button
+          if (_imageFile == null)
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: _takePicture,
+                child: _isCapturing
+                    ? const CircularProgressIndicator()
+                    : const Text('Capture Image'),
+              ),
+            )
+          else
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: ElevatedButton(
+                onPressed: _retakePicture, // Implement _retakePicture logic
+                child: const Text('Retake Image'),
+              ),
+            ),
+
+          // Analyse Image Button, only shows after image is captured
+          if (_imageFile != null)
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: _analyzeImage,
+                child: const Text('Analyze Image'),
+              ),
+            ),
         ],
       ),
     );
